a1f446d94b1245b97d81de70d2e13cd01951bcb8
Konstantinos Tsiligiannis <panekostas@gmail.com>
2026-05-28 13:24:26 +0300

    Update app.c

--- app/app.c
+++ app/app.c
@@ -1,8 +1,65 @@
+#include "app.h"
+#include "scheduler.h"
+#include "uart.h"
+#include "led.h"
+
+volatile uint8_t scheduler_enabled = 0;
+
+void idle_task(void)
+{
+    while(1)
+    {
+
+    }
+}
+
 /*
- * app.c
- *
- *  Created on: 21 Μαΐ 2026
- *      Author: panek
+ * TASK1
+ * LED BLINK TASK
  */
 
+void task1_handler(void)
+{
+    while(1)
+    {
+        led_on();
 
+        uart2_write_string("TASK1 LED ON\r\n");
+
+        task_delay(1000);
+
+        led_off();
+
+        uart2_write_string("TASK1 LED OFF\r\n");
+
+        task_delay(1000);
+    }
+}
+
+/*
+ * UNUSED TASKS
+ */
+
+void task2_handler(void)
+{
+    while(1)
+    {
+        task_delay(1000);
+    }
+}
+
+void task3_handler(void)
+{
+    while(1)
+    {
+        task_delay(1000);
+    }
+}
+
+void task4_handler(void)
+{
+    while(1)
+    {
+        task_delay(1000);
+    }
+}
