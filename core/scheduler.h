551b3ee406b8f0890b3c73e89606ed3a144de10f
Konstantinos Tsiligiannis <panekostas@gmail.com>
2026-05-28 13:25:42 +0300

    Update scheduler.h

--- core/scheduler.h
+++ core/scheduler.h
@@ -1,13 +1,65 @@
-/*
- * scheduler.h
- *
- *  Created on: 21 Μαΐ 2026
- *      Author: panek
- */
-
 #ifndef SCHEDULER_H_
 #define SCHEDULER_H_
 
+#include <stdint.h>
 
+#define MAX_TASKS              5
 
-#endif /* SCHEDULER_H_ */
+#define TASK_READY_STATE       0x00
+#define TASK_BLOCKED_STATE     0xFF
+
+#define DUMMY_XPSR             0x01000000U
+
+#define TICK_HZ                1000U
+
+#define SIZE_TASK_STACK        256U
+#define SIZE_SCHED_STACK       256U
+
+#define SRAM_START             0x20000000U
+#define SIZE_SRAM              (9*1024)
+
+#define SRAM_END               (SRAM_START + SIZE_SRAM)
+
+#define T1_STACK_START         SRAM_END
+#define T2_STACK_START         (SRAM_END-(1*SIZE_TASK_STACK))
+#define T3_STACK_START         (SRAM_END-(2*SIZE_TASK_STACK))
+#define T4_STACK_START         (SRAM_END-(3*SIZE_TASK_STACK))
+#define IDLE_STACK_START       (SRAM_END-(4*SIZE_TASK_STACK))
+#define SCHED_STACK_START      (SRAM_END-(5*SIZE_TASK_STACK))
+
+#define HSI_CLOCK              16000000U
+#define SYSTICK_TIM_CLK        HSI_CLOCK
+
+#define INTERRUPT_DISABLE() \
+do{ \
+__asm volatile("MOV R0,#0x1"); \
+__asm volatile("MSR PRIMASK,R0"); \
+}while(0)
+
+#define INTERRUPT_ENABLE() \
+do{ \
+__asm volatile("MOV R0,#0x0"); \
+__asm volatile("MSR PRIMASK,R0"); \
+}while(0)
+
+typedef struct
+{
+    uint32_t psp_value;
+
+    uint32_t block_count;
+
+    uint8_t current_state;
+
+    void (*task_handler)(void);
+
+}TCB_t;
+
+extern volatile uint32_t g_tick_count;
+
+void scheduler_init(void);
+
+void task_delay(uint32_t tick_count);
+
+void schedule(void);
+
+#endif
