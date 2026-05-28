0d5ab30a1c1ec77e9b2a1e7a9250643779ad5f14
Konstantinos Tsiligiannis <panekostas@gmail.com>
2026-05-28 13:24:49 +0300

    Update cli.c

--- app/cli.c
+++ app/cli.c
@@ -1,11 +1,14 @@
 #include "cli.h"
 #include "uart.h"
 #include "led.h"
+#include "scheduler.h"
 #include <string.h>
 
-typedef struct{
-	const char *name;
-	void (*handler)(void);
+typedef struct
+{
+    const char *name;
+    void (*handler)(void);
+
 }CLI_Command;
 
 void cmd_help(void)
@@ -15,7 +18,8 @@ void cmd_help(void)
         "help\r\n"
         "status\r\n"
         "led on\r\n"
-        "led off\r\n");
+        "led off\r\n"
+        "blink\r\n");
 }
 
 void cmd_status(void)
@@ -27,6 +31,7 @@ void cmd_status(void)
 void cmd_led_on(void)
 {
     led_on();
+
     uart2_write_string(
         "\r\nLED ON\r\n");
 }
@@ -34,8 +39,19 @@ void cmd_led_on(void)
 void cmd_led_off(void)
 {
     led_off();
+
     uart2_write_string(
         "\r\nLED OFF\r\n");
+}
+
+void cmd_blink(void)
+{
+    uart2_write_string(
+        "\r\nStarting Scheduler...\r\n");
+
+    scheduler_init();
+
+    while(1);
 }
 
 CLI_Command commands[] =
@@ -43,21 +59,25 @@ CLI_Command commands[] =
     {"help",cmd_help},
     {"status",cmd_status},
     {"led on",cmd_led_on},
-    {"led off",cmd_led_off}
+    {"led off",cmd_led_off},
+    {"blink",cmd_blink}
 };
 
 void cli_process(char *cmd)
 {
-    uint32_t n = sizeof(commands)/sizeof(commands[0]);
+    uint32_t n =
+        sizeof(commands)/sizeof(commands[0]);
 
     for(uint32_t i=0;i<n;i++)
     {
         if(strcmp(cmd,commands[i].name)==0)
         {
             commands[i].handler();
+
             return;
         }
     }
 
-    uart2_write_string("\r\nUnknown command\r\n");
+    uart2_write_string(
+        "\r\nUnknown command\r\n");
 }
